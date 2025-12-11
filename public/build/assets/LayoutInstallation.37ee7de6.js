baa6e79ab59978ef47d2ac5e29fd1b5669f4bf2a
uduv <ulysseduvillier@gmail.com>
2025-12-11 14:25:02 -0500

    exclude vendor

---
+++ public/build/assets/LayoutInstallation.37ee7de6.js
@@ -0,0 +1,1 @@
+import{N as t}from"./NotificationRoot.de959a6a.js";import{r as s,o as a,e as r,f as o,h as c}from"./vendor.097eeffa.js";import"./main.df0ff204.js";const n={class:"h-screen overflow-y-auto text-base"},i={class:"container mx-auto px-4"},u={setup(f){return(_,m)=>{const e=s("router-view");return a(),r("div",n,[o(t),c("div",i,[o(e)])])}}};export{u as default};
